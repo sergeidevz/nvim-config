9261f5f3ecd0a187bad4dd3585e90ce3e56334e2
Sergei Johansen <sergeij@stud.ntnu.no>
2024-06-20 21:33:43 +0200

    add keymaps for nvimtree

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -6,3 +6,7 @@ vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<CR>', { no
 vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', { noremap = true, silent = true })
+
+
+-- Nvim tree mappings 
+vim.api.nvim_set_keymap('n', '<C-n>', '<cmd>NvimTreeToggle<CR>', {noremap = true, silent = true})
