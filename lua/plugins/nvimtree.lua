e31b2a33b16752ce1380ef5e7c51490794ba480d
Sergei Johansen <sergei.johansen@protonmail.com>
2026-02-12 11:51:27 +0000

    remove nvimtree filters

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,83 +1,83 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	cmd = { "NvimTreeToggle", "NvimTreeFindFileToggle" },
-	dependencies = "nvim-tree/nvim-web-devicons",
-	config = function()
-		local nvimtree = require("nvim-tree")
+  "nvim-tree/nvim-tree.lua",
+  cmd = { "NvimTreeToggle", "NvimTreeFindFileToggle" },
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    local nvimtree = require("nvim-tree")
 
-		-- recommended settings from nvim-tree documentation
-		vim.g.loaded_netrw = 1
-		vim.g.loaded_netrwPlugin = 1
+    -- recommended settings from nvim-tree documentation
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
 
-		nvimtree.setup({
-			view = {
-				width = 35,
-				relativenumber = false,
-			},
-			renderer = {
-				indent_markers = {
-					enable = true,
-				},
-				icons = {
-					glyphs = {
-						default = "",
-						symlink = "",
-						bookmark = "󰆤",
-						modified = "●",
-						folder = {
-							arrow_closed = "",
-							arrow_open = "",
-							default = "",
-							open = "",
-							empty = "",
-							empty_open = "",
-							symlink = "",
-							symlink_open = "",
-						},
-						git = {
-							unstaged = "",
-							staged = "",
-							unmerged = "",
-							renamed = "",
-							untracked = "",
-							deleted = "",
-							ignored = "",
-						},
-					},
-				},
-			},
-			-- disable window_picker for
-			-- explorer to work well with
-			-- window splits
-			actions = {
-				open_file = {
-					window_picker = {
-						enable = true,
-					},
-				},
-			},
-			diagnostics = {
-				enable = false,
-				show_on_dirs = false,
-				show_on_open_dirs = false,
-				debounce_delay = 50,
-				severity = {
-					min = vim.diagnostic.severity.INFO,
-					max = vim.diagnostic.severity.ERROR,
-				},
-				icons = {
-					hint = "",
-					info = "",
-					warning = "",
-					error = "",
-				},
-			},
-			filters = {
-				custom = { ".DS_Store", "node_modules", ".idea", ".git", "vendor" },
-			},
-			git = {
-				ignore = false
-			},
-		})
-	end,
+    nvimtree.setup({
+      view = {
+        width = 35,
+        relativenumber = false,
+      },
+      renderer = {
+        indent_markers = {
+          enable = true,
+        },
+        icons = {
+          glyphs = {
+            default = "",
+            symlink = "",
+            bookmark = "󰆤",
+            modified = "●",
+            folder = {
+              arrow_closed = "",
+              arrow_open = "",
+              default = "",
+              open = "",
+              empty = "",
+              empty_open = "",
+              symlink = "",
+              symlink_open = "",
+            },
+            git = {
+              unstaged = "",
+              staged = "",
+              unmerged = "",
+              renamed = "",
+              untracked = "",
+              deleted = "",
+              ignored = "",
+            },
+          },
+        },
+      },
+      -- disable window_picker for
+      -- explorer to work well with
+      -- window splits
+      actions = {
+        open_file = {
+          window_picker = {
+            enable = true,
+          },
+        },
+      },
+      diagnostics = {
+        enable = false,
+        show_on_dirs = false,
+        show_on_open_dirs = false,
+        debounce_delay = 50,
+        severity = {
+          min = vim.diagnostic.severity.INFO,
+          max = vim.diagnostic.severity.ERROR,
+        },
+        icons = {
+          hint = "",
+          info = "",
+          warning = "",
+          error = "",
+        },
+      },
+      filters = {
+        custom = { ".DS_Store", "node_modules", ".idea" },
+      },
+      git = {
+        ignore = false
+      },
+    })
+  end,
 }
